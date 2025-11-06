3bf870d3267603352b4d5b844044eaf74c64e315
coehgns <modooboiroo@gmail.com>
2025-11-06 23:23:21 +0900

    first commit

---
+++ src/main/kotlin/com/example/bustudy/serveserver/BuStudyServeServerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.bustudy.serveserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BuStudyServeServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<BuStudyServeServerApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/bustudy/serveserver/BuStudyServeServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.bustudy.serveserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BuStudyServeServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
