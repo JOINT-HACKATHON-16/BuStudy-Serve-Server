991572c540700922ad89eef8b2d0de1dcaa21120
coehgns <modooboiroo@gmail.com>
2025-11-06 23:43:04 +0900

    feat: ErrorCode.kt

---
+++ src/main/kotlin/com/example/bustudy/serveserver/global/error/exception/ErrorCode.kt
@@ -0,0 +1,13 @@
+package com.example.hackaton16.global.error.exception
+
+enum class ErrorCode(
+    val status: Int,
+    val message: String,
+) {
+    FEIGN_BAD_REQUEST(400, "Feign Bad Request"),
+    FEIGN_UNAUTHORIZED(401, "Feign UnAuthorized"),
+    FEIGN_FORBIDDEN(403, "Feign Forbidden"),
+    FEIGN_SERVER_ERROR(500, "Feign Server Error"),
+
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error"),
+}
