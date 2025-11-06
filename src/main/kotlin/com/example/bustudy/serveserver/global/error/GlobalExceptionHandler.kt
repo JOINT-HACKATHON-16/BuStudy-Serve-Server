29d63683a67a219bf166838f402dd11e82e3ef29
coehgns <modooboiroo@gmail.com>
2025-11-06 23:44:19 +0900

    feat: GlobalExceptionHandler

---
+++ src/main/kotlin/com/example/bustudy/serveserver/global/error/GlobalExceptionHandler.kt
@@ -0,0 +1,32 @@
+package com.example.hackaton16.global.error
+
+import com.example.hackaton16.global.error.exception.HackatonException
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun validatorExceptionHandler(e: MethodArgumentNotValidException): ResponseEntity<ErrorResponse> {
+        return ResponseEntity(
+            ErrorResponse(
+                400,
+                e.bindingResult.allErrors[0].defaultMessage,
+            ),
+            HttpStatus.BAD_REQUEST,
+        )
+    }
+
+    @ExceptionHandler(HackatonException::class)
+    fun handlingHackatonException(e: HackatonException): ResponseEntity<ErrorResponse> {
+        val code = e.errorCode
+        return ResponseEntity(
+            ErrorResponse(code.status, code.message),
+            HttpStatus.valueOf(code.status),
+        )
+    }
+}
