d25a89e679021f5777d81c838a6955c1ee3fb28f
coehgns <modooboiroo@gmail.com>
2025-11-06 23:43:52 +0900

    feat: FeignUnAuthorizedException

---
+++ src/main/kotlin/com/example/bustudy/serveserver/infrastructure/feign/exception/FeignUnAuthorizedException.kt
@@ -0,0 +1,8 @@
+package com.example.hackaton16.infrastructure.feign.exception
+
+import com.example.hackaton16.global.error.exception.ErrorCode
+import com.example.hackaton16.global.error.exception.HackatonException
+
+object FeignUnAuthorizedException : HackatonException(
+    ErrorCode.FEIGN_UNAUTHORIZED
+)
